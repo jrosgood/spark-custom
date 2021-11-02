203f4d18c8bfb357bb156abde17a942d21416323
josgood <josgood@redhat.com>
2021-11-01 20:48:31 -0400

    Updated build script to add MAVEN_OPTS to prevent heap issues and stack overflows

--- build.sh
+++ build.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+# See https://spark.apache.org/docs/latest/building-spark.html for more info
 while getopts s:h:c:t: flag
 do
     case "${flag}" in
@@ -8,19 +9,17 @@ do
         t) to_directory=${OPTARG};;
     esac
 done
-
 set -ex
+# The MAVEN_OPTS settings below are required to prevent heap errors and stack overflows
+export MAVEN_OPTS="-Xss64m -Xmx2g -XX:ReservedCodeCacheSize=1g"
 cd /tmp
 git clone https://github.com/apache/spark.git spark_src
 cd spark_src
 git checkout "tags/v$spark_version" -b "v$spark_version"
 ./dev/make-distribution.sh --name hadoop$hadoop_version-scala$scala_version --pip --tgz -Phive -Phive-thriftserver -Pmesos -Pyarn -Pkubernetes -Dhadoop.version=$hadoop_version
 fname=spark-$spark_version-bin-hadoop$hadoop_version-scala$scala_version
-directory=$to_directory/$fname
-echo "Moving to directory $directory"
-mv /tmp/spark_src/dist "$directory"
-cd $directory
-echo "Creating archive $fname.tgz"
-tar -cvzf "$fname.tgz" "$directory"
+cp /tmp/spark_src/$fname.tgz $to_directory/$fname.tgz
+# cleanup
+rm -rf /tmp/spark_src
 
 
